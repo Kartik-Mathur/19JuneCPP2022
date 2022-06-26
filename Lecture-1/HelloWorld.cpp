1b23d32ee158dd26d21e7ccb1de4dbfacaf01761
kartik mathur <kartik.mathur10@gmail.com>
2022-06-26 20:04:49 +0530

    Lecture-1

---
+++ Lecture-1/HelloWorld.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int x;
+	cout << x << endl;
+
+	// Intialization:Bucket banate time usmei value dena
+	int a = 10;
+
+	// Assignment: Bucket bani hui hai usse value dedo
+	int y;
+	y = 20;
+
+	// cout << "Hello World";
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lecture-1/input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// int n;
+	// int r;
+	int n, r;
+	// cin >> n;
+	// cin >> r;
+	cin >> n >> r;
+
+	// cout << "User ne enter kia: ";
+	// cout << n;
+	cout << "User ne enter kia: " << n << '\n';
+	cout << "Value of r: " << r << '\n';
+
+	return 0;
+}
